5b8d2b510b13cbce65ee6955e5b1802a6176621b
Joey Maffiola <7maffiolajoey@gmail.com>
2024-11-08 09:12:03 -0600

    First commit.

---
+++ jccPrinters.ps1
@@ -0,0 +1,5 @@
+Add-Printer -ConnectionName "\\JCVS6BDC01\JCCP02"
+Add-Printer -ConnectionName "\\JCVS6BDC01\JCCP03"
+Add-Printer -ConnectionName "\\JCVS6BDC01\JCCP04"
+Get-Printer
+Read-Host -Prompt "Press Enter to exit"
